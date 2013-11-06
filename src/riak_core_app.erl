39ce4a4d3648f1ed8efbb6adf5ae33efa9739c9a
Petr Kozorezov <petr.kozorezov@gmail.com>
2013-11-06 18:29:10 +0400

    web part with webmachine removed and ranch git switched to extend/ranch

--- src/riak_core_app.erl
+++ src/riak_core_app.erl
@@ -100,8 +100,7 @@ start(_StartType, _StartArgs) ->
 prep_stop(_State) ->
     try %% wrap with a try/catch - application carries on regardless,
         %% no error message or logging about the failure otherwise.
-        lager:info("Stopping application riak_core - disabling web services.\n", []),
-        riak_core_sup:stop_webs()
+        lager:info("Stopping application riak_core - disabling web services.\n", [])
     catch
         Type:Reason ->
             lager:error("Stopping application riak_core - ~p:~p.\n", [Type, Reason])
